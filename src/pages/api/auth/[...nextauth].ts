81b08a52171153b28c84b1f7657640c8a3884637
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-06-16 12:32:20 -0300

    Adding create user in database function when login is made

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,46 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  /* In production generate a real key and use it
+  jwt: {
+      signingKey: process.env.SIGNIN_KEY,
+  },
+  */
+  callbacks: {
+      async signIn(user, account, profile){
+        const { email } = user;
+
+        try{
+            await fauna.query(
+                q.If(
+                    q.Not(
+                        q.Exists(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        ) 
+                    ),
+                    // If true
+                    q.Create(
+                        q.Collection('users'),{ data: { email } }
+                    ),
+                    // Else
+                    q.Get(
+                        q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+                    )
+                ),
+            )
+            /*
+            await fauna.query(
+                q.Create(q.Collection('users'),{ data: { email } })
+            );*/
+            return true;
+        }catch {
+            return false;
+        }
+
+        
+      }
+  }
 })
