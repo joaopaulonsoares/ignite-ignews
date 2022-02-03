e018a7b6a65aad1535f12093549af4854505503e
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2022-02-03 09:56:15 -0300

    Removing unused imports

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
-import Link from 'next/link'
 import { SignInButton } from '../SignInButton';
 import { ActiveLink } from '../ActiveLink'
 import styles from './styles.module.scss';
 
 export function Header() {
     
-
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
